e4173d3778aa1ab0ae9f8ea6b26db683091fdb3d
ASO1801007 <1801007@s.asojuku.ac.jp>
2020-09-29 13:09:33 +0900

    migration1

---
+++ loveingapp/database/migrations/2020_09_29_024527_timetypes.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Timetypes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timetypes', function (Blueprint $table) {
+            /* 属性達の定義 */
+            $table->bigIncrements('id',20);
+            $table->string('name',20);
+            $table->time('startTime');
+            $table->time('endTime');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('timetypes');
+    }
+}
